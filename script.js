c6d06e27e65aa0be1d479546e4380153f2296e28
sandraSB0017 <sbarteau@gmail.com>
2022-06-15 20:30:14 +0200

    JS AddEventListener

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
-console.log('connecté');
+
 let btn = document.querySelector('.btn');
-console.log(btn);
 let divSwitch = document.querySelector('.switch');
-console.log(divSwitch);
 let icon = document.querySelector('i');
-console.log(icon);
 let texte = document.querySelector('h1');
-console.log(texte);
 let fond = document.querySelector('.container');
-console.log(fond);
 
 divSwitch.addEventListener('click', function(){
     console.log('cliquée')
     btn.classList.toggle('btn-change');
-    
+    icon.classList.toggle('icon-change');
+    icon.classList.toggle ('fa-sun');
+    fond.classList.toggle('container-change');
+    texte.classList.toggle('text-change');
+    if(texte.innerText === "DarkMode"){
+        texte.innerText = "LightMode";
+    }else {
+        texte.innerText ='DarktMode';
+    }
+    divSwitch.classList.toggle('switch-change');  
 })
 
